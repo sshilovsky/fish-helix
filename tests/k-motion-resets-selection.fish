7bf32331294f945ceff8b816e86b6e281c8d03ca
Sergei Shilovsky <sshilovsky@gmail.com>
2022-09-05 03:46:24 +0300

    add more (broken) tests for `k` motion

---
+++ tests/k-motion-resets-selection.fish
@@ -0,0 +1,5 @@
+# Example test
+# Key input:
+set -g _input qwer Normal o asdf Normal gh k
+# Expected state:
+set -g _selection "q"
---
+++ tests/k-search-continuous.fish
@@ -0,0 +1,5 @@
+# Example test
+# Key input:
+set -g _input printf Enter echo Ender Normal kk
+# Expected state:
+set -g _buffer "printf"
