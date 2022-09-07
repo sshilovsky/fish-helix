945230149fcb51f0b8cc0b45fdafbeb8dec6bc80
Sergei Shilovsky <sshilovsky@gmail.com>
2022-09-07 10:41:23 +0300

    f/t tests

---
+++ tests/ft/count-f.fish
@@ -0,0 +1,5 @@
+# Key input:
+set _input abacaba Normal gh 2fa
+set _broken true
+# Expected state:
+set _selection abaca
---
+++ tests/ft/f-space-inclusive.fish
@@ -0,0 +1,5 @@
+# Key input:
+set _input abacaba Normal Fb
+set _broken true
+# Expected state:
+set _selection "ba "
